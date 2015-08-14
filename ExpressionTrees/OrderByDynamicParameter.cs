25e4dd98b86889fdbe6a0cbe76654f542aa4716e
ivan.i <ivan.i@sbtech.com>
2015-08-14 10:36:08 +0300

    first proj expression trees

---
+++ ExpressionTrees/OrderByDynamicParameter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTrees
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime JoiningDate { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}, Age: {1}, Joining: {2}", Name, Age, JoiningDate);
+        }
+    }
+
+    public class PersonCollection
+    {
+        public List<Person> People { get; set; }
+
+        public PersonCollection()
+        {
+            this.People = new List<Person>();
+            Person p1 = new Person { Name = "Kayes", Age = 29, JoiningDate = DateTime.Parse("2010-06-06") };
+            Person p2 = new Person { Name = "Gibbs", Age = 34, JoiningDate = DateTime.Parse("2008-04-23") };
+            Person p3 = new Person { Name = "Steyn", Age = 28, JoiningDate = DateTime.Parse("2011-02-17") };
+
+            People.Add(p1);
+            People.Add(p2);
+            People.Add(p3);
+        }
+
+        public void SortByPropName(string sortByPropName)
+        {
+            Type sortByPropType = typeof(Person).GetProperty(sortByPropName).PropertyType;
+
+            ParameterExpression personParam = Expression.Parameter(typeof(Person), "p");
+            //var sortExpression = Expression.Lambda<Func<Person, int>>(Expression.Property(personParam, sortByPropName), personParam);
+            LambdaExpression sortExpression = Expression.Lambda(Expression.Property(personParam, sortByPropName), personParam);
+            IQueryable<Person> peopleQery = this.People.AsQueryable<Person>();
+
+            MethodInfo orderByMethodInfo = typeof(Queryable).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Single(mi => mi.Name == "OrderBy" && mi.IsGenericMethodDefinition
+                    && mi.GetGenericArguments().Length == 2
+                    && mi.GetParameters().Length == 2);
+
+            this.People = (orderByMethodInfo.MakeGenericMethod(new Type[] { typeof(Person), sortByPropType }).Invoke(peopleQery,
+            new object[] { peopleQery, sortExpression }) as IOrderedQueryable<Person>).ToList();
+        }
+    }
+}
---
+++ ExpressionTrees/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonCollection people = new PersonCollection();
+            // read prop name to sort by
+            string propName = Console.ReadLine();
+            people.SortByPropName(propName);
+            foreach (var item in people.People)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.Read();
+        }
+
+        ///<summary>
+        ///call in main
+        ///Console.WriteLine(TypeResutFormatter(ExpressionsUtil.ConcatWithSpace, "Ivan", "Ivanov"));
+        ///Console.WriteLine(TypeResutFormatter(ExpressionsUtil.SumIntInDouble, 3, 8));
+        ///</summary>
+        public static string TypeResutFormatter<A, B, R>(Func<A, B, R> func, A a, B b)
+        {
+            R funcRes = func.Invoke(a, b);
+
+            if (typeof(R) == typeof(string))
+            {
+                return String.Format("String: {0}", funcRes.ToString());
+            }
+
+            if (typeof(R) == typeof(double))
+            {
+
+                double res = (double)Convert.ChangeType(funcRes, typeof(R));
+                res += 0.0001d;
+                return String.Format("Double: {0:00.00}", res);
+            }
+
+            return funcRes.ToString();
+        }
+    }
+
+
+
+    public static class ExpressionsUtil
+    {
+        public static Func<string, string, string> ConcatWithSpace = (a, b) => a + ' ' + b;
+
+        public static Func<int, int, double> SumIntInDouble = (a, b) => a + b;
+
+        //Just a simple lambda expression
+        public static Func<int, int, bool> LessThanCompare = (a, b) => a < b;
+
+        private static Expression<Func<int, int, int>> Plus = (a, b) => a + b;
+
+        // Lambda expression as data in the form of an expression tree.
+        public static Expression<Func<int, int, bool>> lessThan = (a, b) => a < b;
+
+        public static Func<int, int, bool> LessThan
+        {
+            get
+            {
+                // Compile the expression tree into executable code.
+                return lessThan.Compile();
+            }
+        }
+    }
+}
