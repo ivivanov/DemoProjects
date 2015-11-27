c32859370a70b3cc7c821e78aaf933b10c52ebef
ivan.i <ivan.i@sbtech.com>
2015-11-27 11:55:28 +0200

    Parallel tests

---
+++ AnyKindOfTests/AsyncTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AnyKindOfTests
+{
+    public class AsyncTests
+    {
+        readonly ParallelOptions n = new ParallelOptions() { MaxDegreeOfParallelism = 5 };
+        static object locakable;
+
+        public void AddAsync(IEnumerable<string> items)
+        {
+            ConcurrentDictionary<string, string> proced = new ConcurrentDictionary<string, string>();
+            List<int> a = new List<int>();
+            Parallel.ForEach(items, n, async item =>
+            {
+                await Task.Delay(100);
+                item += "1";
+                proced.TryAdd(item + "a", item);
+            });
+        }
+
+        public async Task aaa(IEnumerable<string> items)
+        {
+            Task<List<string>> t = Task.Run(() =>
+            {
+                var result = new List<string>();
+                Parallel.ForEach(items,
+                    () => new List<string>(),
+                    (item, loopState, localResult) =>
+                    {
+                        item += "1";
+                        localResult.Add(item + "a");
+                        return localResult;
+                    },
+                    finalLocalResult =>
+                    {
+                        lock (result)
+                        {
+                            result.AddRange(finalLocalResult);
+                        }
+                    });
+
+                return result;
+            });
+
+            await t.ContinueWith(r =>
+            {
+                var result = r.Result;
+
+            });
+
+        }
+    }
+}
--- AnyKindOfTests/Program.cs
+++ AnyKindOfTests/Program.cs
@@ -38,8 +38,34 @@ namespace AnyKindOfTests
     {
         static void Main(string[] args)
         {
-            var z = new BMW();
-            z.ZapaliKola();
+            //var z = new BMW();
+            //z.ZapaliKola();
+            //Console.Read();
+            //long key1 = 1 << 33;
+            //long key2 = int.MaxValue;
+            //Console.WriteLine("{0} key1", Convert.ToString(key1, 2).PadLeft(64, '0'));
+            //Console.WriteLine("{0} key2", Convert.ToString(key2, 2).PadLeft(64, '0'));
+            //Console.WriteLine("{0} key1 shifted back", Convert.ToString(key1 >> 30, 2).PadLeft(64, '0'));
+            //long composite = key1;
+            //Console.WriteLine("{0} composite", Convert.ToString(composite, 2).PadLeft(64, '0'));
+            //composite <<= 31;
+            //Console.WriteLine("{0} composite", Convert.ToString(composite, 2).PadLeft(64, '0'));
+            //composite = composite | key2;
+            int QAParameter1 = 157867129;
+            int QAParameter2 = 157867121;
+            long composite = (long)QAParameter2 << 32 | QAParameter1;
+            //Key	678034121966541945	long
+
+            Console.WriteLine(composite>>32);
+            Console.WriteLine(composite & int.MaxValue);
+            Console.WriteLine("{0} composite", Convert.ToString(int.MaxValue, 2));
+            Console.WriteLine("{0} composite", Convert.ToString(long.MaxValue, 2));
+            //long mask = 1 << 32;
+            //Console.WriteLine("{0} key2 decoded", composite & mask);
+            //composite >>= 32;
+            //Console.WriteLine(composite);
+
+            //Console.WriteLine("{0} key1 decoded", composite & mask);
             Console.Read();
         }
     }
