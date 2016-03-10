4c9598258dd5d72756ac6d54d8da4817022f6db3
ivan.i <ivan.i@sbtech.com>
2016-03-10 18:54:57 +0200

    swagger proj

---
+++ Swagger/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using Swashbuckle.Application;
+
+[assembly: OwinStartup(typeof(Swagger.Startup))]
+
+namespace Swagger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            //SwaggerConfig.Register();
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            httpConfiguration.EnableSwagger(c => c.SingleApiVersion("v1", "A title for your API")).EnableSwaggerUi();
+            httpConfiguration.EnsureInitialized();
+        }
+    }
+}
