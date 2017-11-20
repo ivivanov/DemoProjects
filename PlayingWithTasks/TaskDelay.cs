599dda9f2b9e51166e77e44af03cf6e37c818e50
ivanttyy <ivanivanov12@gmail.com>
2017-11-20 11:48:23 +0200

    play with task dealy

---
+++ PlayingWithTasks/TaskDelay.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlayingWithTasks
+{
+    class TaskDelay
+    {
+        public static void Start()
+        {
+            ServiceA a = new ServiceA();
+            ServiceB b = new ServiceB();
+            ServiceC c = new ServiceC();
+
+            int maxDelay = 3000;
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Task taskA = Task.Delay(maxDelay - a.ResponseTime).ContinueWith(x =>
+            {
+                Console.WriteLine("a");
+                a.DoWork();
+                Console.WriteLine($"taskA ready for {sw.Elapsed}");
+            });
+
+            Task taskB = Task.Delay(maxDelay - b.ResponseTime).ContinueWith(x =>
+            {
+                Console.WriteLine("b");
+                b.DoWork();
+                Console.WriteLine($"taskB ready for {sw.Elapsed}");
+            });
+
+            Task taskC = Task.Delay(maxDelay - c.ResponseTime).ContinueWith(x =>
+            {
+                Console.WriteLine("c");
+                c.DoWork();
+                Console.WriteLine($"taskC ready for {sw.Elapsed}");
+            });
+
+            Task.WaitAll(new Task[] { taskA, taskB, taskC});
+
+            //taskA.Wait();
+            //taskB.Wait();
+            //taskC.Wait();
+        }
+    }
+
+
+
+    class ServiceA
+    {
+        public int ResponseTime { get => 500; }
+        public void DoWork() => Thread.Sleep(500);
+    }
+
+    class ServiceB
+    {
+        public int ResponseTime { get => 1000; }
+        public void DoWork() => Thread.Sleep(1000);
+    }
+
+    class ServiceC
+    {
+        public int ResponseTime { get => 3000; }
+        public void DoWork() => Thread.Sleep(3000);
+    }
+}
---
+++ TaskDelay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskDelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
