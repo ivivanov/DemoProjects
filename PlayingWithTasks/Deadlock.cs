6232a453983cff5834d64313bf9f87c0700fff23
ivanttyy <ivanivanov12@gmail.com>
2017-12-01 11:15:53 +0200

    deadlock demo

--- PlayingWithTasks/Deadlock.cs
+++ PlayingWithTasks/Deadlock.cs
@@ -7,23 +7,13 @@ using System.Threading.Tasks;
 
 namespace PlayingWithTasks
 {
-    class Program
+    public class Deadlock
     {
-        private static object locker1 = new object();
-        private static object locker2 = new object();
+        private readonly object locker1 = new object();
+        private readonly object locker2 = new object();
 
-        static void Main(string[] args)
+        public void Start()
         {
-            //TaskDelay.Start();
-            //new TimerAndTasks().Start();
-            //new Threads().Start();
-            //new ThreadsConcurency().Start();
-            //new TwoThreadsSameData().Start();
-            //new ForeGRAndBGR().Start();
-            //new Deadlock().Start();
-
-            //Console.WriteLine("end");
-
             new Thread(() =>
             {
                 Console.WriteLine("hello from NT");
